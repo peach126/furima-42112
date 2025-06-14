be496c29297d2e7cac14575f2600f072b49bb100
peach126 <dmlc0915@gmail.com>
2025-06-14 21:39:05 +0900

    ユーザー管理機能の修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,9 +4,6 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  has_many :items
-  has_many :orders
-
   PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i
   validates_format_of :password, with: PASSWORD_REGEX
 
