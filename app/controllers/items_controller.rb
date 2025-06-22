a2a1410a30ee86d7e1c55433e161470736d46fd8
peach126 <dmlc0915@gmail.com>
2025-06-22 15:37:23 +0900

    商品出品機能の修正2

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,9 +2,9 @@ class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
   before_action :contributor_confirmation, only: [:edit, :update, :destroy]
   def index
-    @item = Item.all
-    @item = Item.includes(:user)
-    @item = Item.order('created_at DESC')
+    # @item = Item.all
+    # @item = Item.includes(:user)
+    # @item = Item.order('created_at DESC')
   end
 
   def new
