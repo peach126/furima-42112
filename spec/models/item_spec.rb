5cfd6843c2dada04322d886cebbfe7a72e219e05
peach126 <dmlc0915@gmail.com>
2025-06-22 15:10:16 +0900

    商品出品機能の修正

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -67,27 +67,27 @@ RSpec.describe Item, type: :model do
         expect(@item.errors.full_messages).to include("Item description can't be blank")
       end
       it 'カテゴリーの情報が空では出品できない' do
-        @item.category_id = nil
+        @item.category_id = 0
         @item.valid?
         expect(@item.errors.full_messages).to include("Category can't be blank")
       end
       it '商品の状態の情報が空では出品できない' do
-        @item.item_condition_id = nil
+        @item.item_condition_id = 0
         @item.valid?
         expect(@item.errors.full_messages).to include("Item condition can't be blank")
       end
       it '配送料の負担が空では出品できない' do
-        @item.shipping_fee_id = nil
+        @item.shipping_fee_id = 0
         @item.valid?
         expect(@item.errors.full_messages).to include("Shipping fee can't be blank")
       end
       it '発送元の地域が空では出品できない' do
-        @item.prefecture_id = nil
+        @item.prefecture_id = 0
         @item.valid?
         expect(@item.errors.full_messages).to include("Prefecture can't be blank")
       end
       it '発送までの日数が空では出品できない' do
-        @item.delivery_time_id = nil
+        @item.delivery_time_id = 0
         @item.valid?
         expect(@item.errors.full_messages).to include("Delivery time can't be blank")
       end
@@ -105,6 +105,11 @@ RSpec.describe Item, type: :model do
         @item.price = 10_000_000
         @item.valid?
         expect(@item.errors.full_messages).to include('Price must be less than or equal to 9999999')
+      end
+      it '価格が半角数字では保存できないこと' do
+        @item.price = '５００'
+        @item.valid?
+        expect(@item.errors.full_messages).to include('Price is not a number')
       end
     end
   end
